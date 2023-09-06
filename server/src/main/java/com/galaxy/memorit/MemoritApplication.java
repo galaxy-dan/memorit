e4c3906c4ae43e8d91fba071452b6cf47369d8e4
chemwang13 <chemwang13@naver.com>
2023-09-06 13:26:42 +0900

    Intialize server

---
+++ server/src/main/java/com/galaxy/memorit/MemoritApplication.java
@@ -0,0 +1,13 @@
+package com.galaxy.memorit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoritApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoritApplication.class, args);
+	}
+
+}
