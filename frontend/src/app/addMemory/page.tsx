5989f795c873c5b1e4e2516e45990e4d5150f4dd
ssh102525 <ssh102525@naver.com>
2023-09-11 17:52:45 +0900

    [S09P22A207-87] feat: 송수신 여부 버튼 페이지에 추가

--- frontend/src/app/addMemory/page.tsx
+++ frontend/src/app/addMemory/page.tsx
@@ -1,10 +1,20 @@
 'use client';
 
-
+import SelectButton from '@/components/selectButton';
+import SelectButtonGroup from '@/components/selectButtonGroup';
+import { useState } from 'react';
 import { IoMdClose } from 'react-icons/io';
 
 export default function AddMemoryPage() {
-  
+  const [send, setSend] = useState<boolean>(true);
+
+  function setSendTrue() {
+    setSend(true);
+  }
+
+  function setSendFalse() {
+    setSend(false);
+  }
 
   return (
     <>
@@ -15,7 +25,21 @@ export default function AddMemoryPage() {
         <button className="text-red-400">저장</button>
       </div>
 
-      
+      {/* 송수신 선택 부분 */}
+      <SelectButtonGroup>
+        <SelectButton
+          isSelected={send}
+          position={'l'}
+          text={'보낸 기억'}
+          onClickFunction={setSendTrue}
+        />
+        <SelectButton
+          isSelected={!send}
+          position={'r'}
+          text={'받은 기억'}
+          onClickFunction={setSendFalse}
+        />
+      </SelectButtonGroup>
     </>
   );
 }
