305d46602a65056c341885099518c174f8b16730
jhp336 <jhp336@naver.com>
2023-09-11 13:49:02 +0900

    [S09P22A207-58] feat: DTO 수정

--- server/src/main/java/com/galaxy/memorit/friend/dto/response/FriendInfoDTO.java
+++ server/src/main/java/com/galaxy/memorit/friend/dto/response/FriendInfoDTO.java
@@ -1,12 +1,14 @@
 package com.galaxy.memorit.friend.dto.response;
 
-import com.galaxy.memorit.friend.domain.entity.Friend;
-
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class FriendInfoDTO {
 	private String userId;
 	private String friendId;
@@ -16,15 +18,4 @@ public class FriendInfoDTO {
 	private int sentCount;
 	private int receivedMoney;
 	private int sentMoney;
-
-	public FriendInfoDTO(Friend friend) {
-		this.userId = new String(friend.getUserId());
-		this.friendId = new String(friend.getFriendId());
-		this.name = friend.getName();
-		this.category = friend.getCategory();
-		this.receivedCount = friend.getReceivedCount();
-		this.sentCount = friend.getSentCount();
-		this.receivedMoney = friend.getReceivedMoney();
-		this.sentMoney = friend.getSentMoney();
-	}
 }
--- server/src/main/java/com/galaxy/memorit/friend/dto/response/FriendsListResDTO.java
+++ server/src/main/java/com/galaxy/memorit/friend/dto/response/FriendsListResDTO.java
@@ -1,21 +1,14 @@
 package com.galaxy.memorit.friend.dto.response;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-import com.galaxy.memorit.friend.domain.entity.Friend;
-
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class FriendsListResDTO {
 	private List<FriendInfoDTO> list;
-
-	public FriendsListResDTO(List<Friend> friendList) {
-		this.list = friendList.stream()
-			.map(FriendInfoDTO::new)
-			.collect(Collectors.toList());
-	}
 }
