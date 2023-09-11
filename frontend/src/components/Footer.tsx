0212207a9ddb164d785a2d8840ed6440e8934455
JeonJungyu <wjswnsrb93@naver.com>
2023-09-11 14:15:03 +0900

    [S09P22A207-94] refactor : footer 컴포넌트 리팩토링(list)

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,22 +1,41 @@
 import { GoHome, GoHomeFill } from 'react-icons/go';
-import { BiPlusCircle } from 'react-icons/bi';
-import { MdOutlinePeopleAlt } from 'react-icons/md';
+import { MdAddCircleOutline } from 'react-icons/md';
+import { MdOutlinePeopleAlt, MdPeopleAlt } from 'react-icons/md';
+import FooterItem from './FooterItem';
+
+const footerList = [
+  {
+    icon: <GoHome size="50" />,
+    clickedIcon: <GoHomeFill size="50" />,
+    path: '/',
+    text: '홈',
+  },
+  {
+    icon: <MdAddCircleOutline size="50" />,
+    clickedIcon: <MdAddCircleOutline size="50" />,
+    path: '/add',
+    text: '기록추가',
+  },
+  {
+    icon: <MdOutlinePeopleAlt size="50" />,
+    clickedIcon: <MdPeopleAlt size="50" />,
+    path: '/friend',
+    text: '친구',
+  },
+];
 
 export default function Footer() {
   return (
     <footer className="fixed bottom-0 max-w-md w-full bg-slate-300 flex px-2 items-start gap-2">
-      <div className="flex flex-col justify-center items-center gap-1 pt-3 pb-4 flex-1">
-        <GoHome size="50" />
-        <div>홈</div>
-      </div>
-      <div className="flex flex-col justify-center items-center gap-1 pt-3 pb-4 flex-1">
-        <GoHome size="50" />
-        <div>홈</div>
-      </div>
-      <div className="flex flex-col justify-center items-center gap-1 pt-3 pb-4 flex-1">
-        <GoHome size="50" />
-        <div>홈</div>
-      </div>
+      {footerList.map(({ icon, clickedIcon, path, text }) => (
+        <FooterItem
+          key={path}
+          icon={icon}
+          clickedIcon={clickedIcon}
+          path={path}
+          text={text}
+        />
+      ))}
     </footer>
   );
 }
