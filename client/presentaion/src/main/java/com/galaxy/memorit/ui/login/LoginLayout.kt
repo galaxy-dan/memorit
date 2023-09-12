159b16f97961f7ed2069036d9729c9ebc1826f61
JINWOO CHOI <csw3055@gmail.com>
2023-09-12 13:25:22 +0900

    [#S09P22A207-63] feat: UI함수화 및 로그인 버튼 추가

--- client/presentaion/src/main/java/com/galaxy/memorit/ui/login/LoginLayout.kt
+++ client/presentaion/src/main/java/com/galaxy/memorit/ui/login/LoginLayout.kt
@@ -1,9 +1,7 @@
 package com.galaxy.memorit.ui.login
 
-import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -20,7 +20,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
@@ -37,7 +36,6 @@ import com.galaxy.memorit.ui.login.stateholder.LoginViewModel
 import com.galaxy.memorit.ui.theme.PartylogTheme
 import com.galaxy.memorit.ui.theme.loginTextColor
 import com.galaxy.memorit.ui.theme.maplestory
-import com.galaxy.memorit.util.Util
 
 @Composable
 fun Login(
@@ -126,64 +124,18 @@ fun Login(
 
         Column(modifier = Modifier
             .align(Alignment.CenterStart)
-            .padding(bottom = 300.dp, start = 40.dp)) {
-            Text(text = stringResource(id = R.string.app_name_kor),
-                style = TextStyle(
-                    fontSize = 35.sp,
-                    lineHeight = 20.sp,
-                    fontWeight = FontWeight(700),
-                    color = Color(0xFF000000),
-                    letterSpacing = 0.35.sp,
-                )
-            )
-            Column(modifier = Modifier.padding(top = 15.dp)) {
-                Box() {
-                    Text(text = stringResource(id = R.string.first_greeting_line1),
-                        style = TextStyle(
-                            fontSize = 18.sp,
-                            lineHeight = 20.sp,
-                            fontWeight = FontWeight(500),
-                            color = Color.Black,
-                            letterSpacing = 0.18.sp))
-                    Text(text = stringResource(id = R.string.first_greeting_line1),
-                        style = TextStyle(
-                            fontSize = 18.sp,
-                            lineHeight = 20.sp,
-                            fontWeight = FontWeight(500),
-                            color = Color.Transparent,
-                            letterSpacing = 0.18.sp),
-                    modifier = Modifier
-                        .background(Color(0x3300B2FF))
-                        .height(9.dp)
-                        .align(Alignment.BottomCenter))
-                }
-                Box() {
-                    Text(
-                        text = stringResource(id = R.string.first_greeting_line2),
-                        style = TextStyle(
-                            fontSize = 18.sp,
-                            lineHeight = 20.sp,
-                            fontWeight = FontWeight(500),
-                            color = Color.Black,
-                            letterSpacing = 0.18.sp
-                        )
-                    )
-                    Text(
-                        text = stringResource(id = R.string.first_greeting_line2),
-                        style = TextStyle(
-                            fontSize = 18.sp,
-                            lineHeight = 20.sp,
-                            fontWeight = FontWeight(500),
-                            color = Color.Transparent,
-                            letterSpacing = 0.18.sp
-                        ),
-                        modifier = Modifier
-                            .background(Color(0x3300B2FF))
-                            .height(9.dp)
-                            .align(Alignment.BottomCenter)
-                    )
-                }
-            }
+            .padding(bottom = 300.dp, start = 40.dp, top = 15.dp)) {
+            Title()
+            SubTitle1()
+            SubTitle2()
+        }
+
+        IconButton(onClick = { } ,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 150.dp)) {
+            Icon(imageVector = ImageVector.vectorResource(id = R.drawable.ic_login_kakaologin),
+                contentDescription = null)
         }
     }
 
@@ -215,26 +167,93 @@ fun Login(
 }
 
 @Composable
-fun LoginFrame(modifier: Modifier, viewModel: LoginViewModel, loginCode: Int,
-               navToGetbirth: () -> Unit, navToMain: () -> Unit) {
-    val context = LocalContext.current
-    when(loginCode) {
-        200 -> navToMain()
-        201 -> navToGetbirth()
-        0 -> {}
-        else -> {
-            Toast.makeText(LocalContext.current, "에러", Toast.LENGTH_SHORT).show()
-        }
+fun Title() {
+    Text(text = stringResource(id = R.string.app_name_kor),
+        style = TextStyle(
+            fontSize = 35.sp,
+            lineHeight = 20.sp,
+            fontWeight = FontWeight(700),
+            color = Color(0xFF000000),
+            letterSpacing = 0.35.sp,
+        )
+    )
+}
+
+@Composable
+fun SubTitle1() {
+    Box() {
+        Text(text = stringResource(id = R.string.first_greeting_line1),
+            style = TextStyle(
+                fontSize = 18.sp,
+                lineHeight = 20.sp,
+                fontWeight = FontWeight(500),
+                color = Color.Black,
+                letterSpacing = 0.18.sp))
+        Text(text = stringResource(id = R.string.first_greeting_line1),
+            style = TextStyle(
+                fontSize = 18.sp,
+                lineHeight = 20.sp,
+                fontWeight = FontWeight(500),
+                color = Color.Transparent,
+                letterSpacing = 0.18.sp),
+            modifier = Modifier
+                .background(Color(0x3300B2FF))
+                .height(9.dp)
+                .align(Alignment.BottomCenter))
     }
-    viewModel.resetCode()
-    Image(imageVector = ImageVector.vectorResource(id = R.drawable.ic_login_kakaologin),
-        contentDescription = "kakao",
-        modifier = modifier
-            .padding(bottom = 124.dp)
-            .clickable {
-                Util(viewModel = viewModel).kakaoLogin(context)
-            })
+}
+
+@Composable
+fun SubTitle2() {
+    Box() {
+        Text(
+            text = stringResource(id = R.string.first_greeting_line2),
+            style = TextStyle(
+                fontSize = 18.sp,
+                lineHeight = 20.sp,
+                fontWeight = FontWeight(500),
+                color = Color.Black,
+                letterSpacing = 0.18.sp
+            )
+        )
+        Text(
+            text = stringResource(id = R.string.first_greeting_line2),
+            style = TextStyle(
+                fontSize = 18.sp,
+                lineHeight = 20.sp,
+                fontWeight = FontWeight(500),
+                color = Color.Transparent,
+                letterSpacing = 0.18.sp
+            ),
+            modifier = Modifier
+                .background(Color(0x3300B2FF))
+                .height(9.dp)
+                .align(Alignment.BottomCenter)
+        )
     }
+}
+
+//@Composable
+//fun LoginFrame(modifier: Modifier, viewModel: LoginViewModel, loginCode: Int,
+//               navToGetbirth: () -> Unit, navToMain: () -> Unit) {
+//    val context = LocalContext.current
+//    when(loginCode) {
+//        200 -> navToMain()
+//        201 -> navToGetbirth()
+//        0 -> {}
+//        else -> {
+//            Toast.makeText(LocalContext.current, "에러", Toast.LENGTH_SHORT).show()
+//        }
+//    }
+//    viewModel.resetCode()
+//    Image(imageVector = ImageVector.vectorResource(id = R.drawable.ic_login_kakaologin),
+//        contentDescription = "kakao",
+//        modifier = modifier
+//            .padding(bottom = 124.dp)
+//            .clickable {
+//                Util(viewModel = viewModel).kakaoLogin(context)
+//            })
+//    }
 
 
 @Composable
