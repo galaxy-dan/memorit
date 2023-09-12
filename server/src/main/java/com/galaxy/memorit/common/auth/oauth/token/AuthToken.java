1e861b604b5601a2aa1b5e56afe9083054b62a98
sjyoon <sjyoon4755@gmail.com>
2023-09-12 14:47:03 +0900

    S09P22A207-110/feat: add authtoken

--- server/src/main/java/com/galaxy/memorit/common/auth/oauth/token/AuthToken.java
+++ server/src/main/java/com/galaxy/memorit/common/auth/oauth/token/AuthToken.java
@@ -1,5 +1,80 @@
 package com.galaxy.memorit.common.auth.oauth.token;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.security.Key;
+import java.util.Date;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
 public class AuthToken {
 
+    @Getter
+    private final String token;
+    private final Key key;
+
+    private static final String AUTHORITIES_KEY = "role";
+
+    AuthToken(String id, Date expiry, Key key) {
+        this.token = createAuthToken(id, expiry);
+        this.key = key;
+    }
+
+    AuthToken(String id, String role, Date expiry, Key key) {
+        this.token = createAuthToken(id, role, expiry);
+        this.key = key;
+    }
+
+    private String createAuthToken(String id, Date expiry) {
+        return Jwts.builder()
+            .setSubject(id)
+            .signWith(key, SignatureAlgorithm.HS256)
+            .setExpiration(expiry)
+            .compact();
+    }
+
+    private String createAuthToken(String id, String role, Date expiry) {
+        return Jwts.builder()
+            .setSubject(id)
+            .claim(AUTHORITIES_KEY, role)
+            .signWith(key, SignatureAlgorithm.HS256)
+            .setExpiration(expiry)
+            .compact();
+    }
+
+    public boolean validate() {
+        return this.getTokenClaims() != null;
+    }
+
+    public Claims getTokenClaims() {
+        try {
+            return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        } catch (SecurityException e) {
+            log.info("Invalid Token");
+        }
+        return null;
+    }
+
+    public Claims getExpiredTokenClaims() {
+        try {
+            Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        } catch (ExpiredJwtException e) {
+            log.info("Expired Token");
+            return e.getClaims();
+        }
+        return null;
+    }
 }
--- server/src/main/java/com/galaxy/memorit/common/auth/oauth/token/AuthTokenProvider.java
+++ server/src/main/java/com/galaxy/memorit/common/auth/oauth/token/AuthTokenProvider.java
@@ -1,5 +1,58 @@
 package com.galaxy.memorit.common.auth.oauth.token;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.User;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+@Slf4j
 public class AuthTokenProvider {
 
+    private final Key key;
+    private static final String AUTHORITIES_KEY = "role";
+
+    public AuthTokenProvider(String secret) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public AuthToken createAuthToken(String id, Date expiry) {
+        return new AuthToken(id, expiry, key);
+    }
+
+    public AuthToken createAuthToken(String id, String role, Date expiry) {
+        return new AuthToken(id, role, expiry, key);
+    }
+
+    public AuthToken convertAuthToken(String token) {
+        return new AuthToken(token, key);
+    }
+
+    public Authentication getAuthentication(AuthToken authToken) {
+
+        if (authToken.validate()) {
+            Claims claims = authToken.getTokenClaims();
+            Collection<? extends GrantedAuthority> authorities
+                = Arrays.stream(new String[] {claims.get(AUTHORITIES_KEY).toString()})
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+
+//            User principal = new User(claims.getSubject())
+
+            return new UsernamePasswordAuthenticationToken(authToken, authorities);
+
+        } else {
+            return null;
+        }
+
+
+    }
 }
