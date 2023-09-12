16aea57ee83d02d72dfe9d4df9f82ff593e05dff
JINWOO CHOI <csw3055@gmail.com>
2023-09-12 12:57:20 +0900

    [#S09P22A207-63] feat: 제목 및 부제목 추가

--- client/presentaion/src/main/java/com/galaxy/memorit/ui/login/LoginLayout.kt
+++ client/presentaion/src/main/java/com/galaxy/memorit/ui/login/LoginLayout.kt
@@ -2,19 +2,21 @@ package com.galaxy.memorit.ui.login
 
 import android.widget.Toast
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
@@ -22,15 +24,12 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.galaxy.memorit.R
@@ -125,22 +124,66 @@ fun Login(
                 .size(randCustomNum(getScreenWidth() / 4).dp))
 
 
-        Column(modifier = Modifier.align(Alignment.CenterStart).padding(bottom = 50.dp)) {
-            Text(text = stringResource(id = R.string.app_name) )
-            Text(text = "소중한 사람들과/n주고받은 기억을 기록해보세요.",
-                style = LocalTextStyle.current.merge(
-                    TextStyle(
-                        lineHeight = 2.5.em,
-                        platformStyle = PlatformTextStyle(
-                            includeFontPadding = false
-                        ),
-                        lineHeightStyle = LineHeightStyle(
-                            alignment = LineHeightStyle.Alignment.Center,
-                            trim = LineHeightStyle.Trim.None
-                        )
-                    )
+        Column(modifier = Modifier
+            .align(Alignment.CenterStart)
+            .padding(bottom = 300.dp, start = 40.dp)) {
+            Text(text = stringResource(id = R.string.app_name_kor),
+                style = TextStyle(
+                    fontSize = 35.sp,
+                    lineHeight = 20.sp,
+                    fontWeight = FontWeight(700),
+                    color = Color(0xFF000000),
+                    letterSpacing = 0.35.sp,
                 )
             )
+            Column(modifier = Modifier.padding(top = 15.dp)) {
+                Box() {
+                    Text(text = stringResource(id = R.string.first_greeting_line1),
+                        style = TextStyle(
+                            fontSize = 18.sp,
+                            lineHeight = 20.sp,
+                            fontWeight = FontWeight(500),
+                            color = Color.Black,
+                            letterSpacing = 0.18.sp))
+                    Text(text = stringResource(id = R.string.first_greeting_line1),
+                        style = TextStyle(
+                            fontSize = 18.sp,
+                            lineHeight = 20.sp,
+                            fontWeight = FontWeight(500),
+                            color = Color.Transparent,
+                            letterSpacing = 0.18.sp),
+                    modifier = Modifier
+                        .background(Color(0x3300B2FF))
+                        .height(9.dp)
+                        .align(Alignment.BottomCenter))
+                }
+                Box() {
+                    Text(
+                        text = stringResource(id = R.string.first_greeting_line2),
+                        style = TextStyle(
+                            fontSize = 18.sp,
+                            lineHeight = 20.sp,
+                            fontWeight = FontWeight(500),
+                            color = Color.Black,
+                            letterSpacing = 0.18.sp
+                        )
+                    )
+                    Text(
+                        text = stringResource(id = R.string.first_greeting_line2),
+                        style = TextStyle(
+                            fontSize = 18.sp,
+                            lineHeight = 20.sp,
+                            fontWeight = FontWeight(500),
+                            color = Color.Transparent,
+                            letterSpacing = 0.18.sp
+                        ),
+                        modifier = Modifier
+                            .background(Color(0x3300B2FF))
+                            .height(9.dp)
+                            .align(Alignment.BottomCenter)
+                    )
+                }
+            }
         }
     }
 
@@ -205,7 +248,7 @@ fun TitleFrame(modifier: Modifier = Modifier, font: FontFamily) {
         contentScale = ContentScale.FillWidth
     )
     Text(
-        text = stringResource(id = R.string.first_greeting),
+        text = stringResource(id = R.string.first_greeting_line1),
         textAlign = TextAlign.Center,
         fontSize = 16.sp,
         fontFamily = font, fontWeight = FontWeight.Light,
