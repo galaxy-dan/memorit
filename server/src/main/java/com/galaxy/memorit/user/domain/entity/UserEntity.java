aece41c4d38997cf78c20acb885e6277ade27fcd
sjyoon <sjyoon4755@gmail.com>
2023-09-12 17:57:33 +0900

    feat: userentity jwtconfig

--- server/src/main/java/com/galaxy/memorit/user/domain/entity/UserEntity.java
+++ server/src/main/java/com/galaxy/memorit/user/domain/entity/UserEntity.java
@@ -1,5 +1,21 @@
 package com.galaxy.memorit.user.domain.entity;
 
 public class UserEntity {
+    private long userId;
+    private String nickname;
+    private int receivedCount;
+    private int sentCount;
+    private int receivedMoney;
+    private int sentMoney;
 
+    public UserEntity(long userId, String nickname, int receivedCount, int sentCount,
+        int receivedMoney,
+        int sentMoney) {
+        this.userId = userId;
+        this.nickname = nickname;
+        this.receivedCount = receivedCount;
+        this.sentCount = sentCount;
+        this.receivedMoney = receivedMoney;
+        this.sentMoney = sentMoney;
+    }
 }
