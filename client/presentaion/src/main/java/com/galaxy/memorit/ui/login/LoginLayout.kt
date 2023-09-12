4a01c93af8541757df2623895486516978c5d4c5
JINWOO CHOI <csw3055@gmail.com>
2023-09-12 10:40:58 +0900

    [#S09P22A207-63] feat: 하트리소스 추가, 랜덤함수 두개로 분리

--- client/presentaion/src/main/java/com/galaxy/memorit/ui/login/LoginLayout.kt
+++ client/presentaion/src/main/java/com/galaxy/memorit/ui/login/LoginLayout.kt
@@ -3,11 +3,11 @@ package com.galaxy.memorit.ui.login
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -44,31 +44,62 @@ fun Login(
 
     AutoLogin {viewModel.autoLogin(navToMain)}
 
-
-    Column() {
-        Row {
-
-        }
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = modifier
-                .weight(1f)
-                .fillMaxWidth(),
-            verticalArrangement = Arrangement.Bottom
-        ) {
-            TitleFrame(modifier, font)
-        }
-
-        Column(
-            modifier = modifier
-                .weight(1f)
-                .fillMaxWidth(), verticalArrangement = Arrangement.Bottom,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            LoginFrame(modifier, viewModel, loginCode, navToGetbirth, navToMain)
-        }
-
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(painter = painterResource(id = R.drawable.red_heart), contentDescription = null,
+            modifier = Modifier.align(Alignment.Center)
+                .padding(start = randHighNum().dp, end = randLowNum().dp, top = randLowNum().dp, bottom = randHighNum().dp)
+                .size(randHighNum().dp))
+        Image(painter = painterResource(id = R.drawable.orange_heart), contentDescription = null,
+            modifier = Modifier.align(Alignment.TopEnd)
+                .padding(start = randLowNum().dp, end = randHighNum().dp, top = randHighNum().dp, bottom = randLowNum().dp)
+                .size(200.dp),
+            contentScale = ContentScale.Fit)
+        Image(painter = painterResource(id = R.drawable.yellow_heart), contentDescription = null,
+            modifier = Modifier.align(Alignment.TopStart)
+                .padding(start = randHighNum().dp, end = randLowNum().dp, top = randLowNum().dp, bottom = randHighNum().dp)
+                .size(randHighNum().dp))
+        Image(painter = painterResource(id = R.drawable.green_heart), contentDescription = null,
+            modifier = Modifier.align(Alignment.CenterStart)
+                .padding(start = randHighNum().dp, end = randLowNum().dp, top = randLowNum().dp, bottom = randHighNum().dp)
+                .size(randHighNum().dp))
+        Image(painter = painterResource(id = R.drawable.blue_heart), contentDescription = null,
+            modifier = Modifier.align(Alignment.CenterEnd)
+                .padding(start = randHighNum().dp, end = randLowNum().dp, top = randLowNum().dp, bottom = randHighNum().dp)
+                .size(randHighNum().dp))
+        Image(painter = painterResource(id = R.drawable.navy_heart), contentDescription = null,
+            modifier = Modifier.align(Alignment.BottomStart)
+                .padding(start = randHighNum().dp, end = randLowNum().dp, top = randLowNum().dp, bottom = randHighNum().dp)
+                .size(randHighNum().dp))
+        Image(painter = painterResource(id = R.drawable.purple_heart), contentDescription = null,
+            modifier = Modifier.align(Alignment.BottomEnd)
+                .padding(start = randHighNum().dp, end = randLowNum().dp, top = randLowNum().dp, bottom = randHighNum().dp)
+                .size(randHighNum().dp))
     }
+
+//    Column() {
+//        Row {
+//
+//        }
+//        Column(
+//            horizontalAlignment = Alignment.CenterHorizontally,
+//            modifier = modifier
+//                .weight(1f)
+//                .fillMaxWidth(),
+//            verticalArrangement = Arrangement.Bottom
+//        ) {
+//            TitleFrame(modifier, font)
+//        }
+//
+//        Column(
+//            modifier = modifier
+//                .weight(1f)
+//                .fillMaxWidth(), verticalArrangement = Arrangement.Bottom,
+//            horizontalAlignment = Alignment.CenterHorizontally
+//        ) {
+//            LoginFrame(modifier, viewModel, loginCode, navToGetbirth, navToMain)
+//        }
+//
+//    }
 
 }
 
@@ -125,8 +156,12 @@ fun AutoLogin(autoLogin: () -> Unit)  {
     }
 }
 
-fun randNum(): Int {
-    val range = (0..100)
+fun randHighNum(): Int {
+    val range = (50..100)
+    return range.random()
+}
+fun randLowNum(): Int{
+    val range = (0..50)
     return range.random()
 }
 
