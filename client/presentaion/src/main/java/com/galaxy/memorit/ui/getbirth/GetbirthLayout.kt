914e0d7a88686cd7c3d04b846d74668060face9f
JINWOO CHOI <csw3055@gmail.com>
2023-09-13 12:53:47 +0900

    [#S09P22A207-90] feat: metrial3 설정

--- client/presentaion/src/main/java/com/galaxy/memorit/ui/getbirth/GetbirthLayout.kt
+++ client/presentaion/src/main/java/com/galaxy/memorit/ui/getbirth/GetbirthLayout.kt
@@ -3,20 +3,25 @@ package com.galaxy.memorit.ui.getbirth
 import android.content.Context
 import android.provider.ContactsContract
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.Checkbox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -29,6 +34,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.chargemap.compose.numberpicker.NumberPicker
+import com.galaxy.domain.model.getPhone.req.PhoneData
 import com.galaxy.memorit.R
 import com.galaxy.memorit.ui.getbirth.stateholder.PickerStateHolder
 import com.galaxy.memorit.ui.theme.maplestory
@@ -100,7 +106,6 @@ fun Title(modifier: Modifier = Modifier) {
                 .padding(top = 20.dp))
     }
 }
-
 @Composable
 fun Picker(modifier: Modifier = Modifier, stateHolder: PickerStateHolder) {
     Logger.d("pickercomp")
