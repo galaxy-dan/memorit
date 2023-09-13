7bd37f3ab54fadf44aa88eb700635d0744fa25a7
JINWOO CHOI <csw3055@gmail.com>
2023-09-13 13:00:16 +0900

    [#S09P22A207-90] feat: 주소록 데이터 관리 로직 추가

--- client/presentaion/src/main/java/com/galaxy/memorit/ui/getbirth/GetbirthViewmodel.kt
+++ client/presentaion/src/main/java/com/galaxy/memorit/ui/getbirth/GetbirthViewmodel.kt
@@ -1,7 +1,9 @@
 package com.galaxy.memorit.ui.getbirth
 
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.galaxy.domain.model.getPhone.req.PhoneData
 import com.orhanobut.logger.Logger
 import com.galaxy.domain.model.login.req.JoinWithBirthReq
 import com.galaxy.domain.usecase.login.GetidUsecase
@@ -13,6 +15,27 @@ import javax.inject.Inject
 @HiltViewModel
 class GetbirthViewmodel @Inject constructor(private val joinWithBirthUsecase: JoinWithBirthUsecase,
     private val getidUsecase: GetidUsecase): ViewModel() {
+
+
+    private val _phoneData = mutableStateListOf<PhoneData>()
+    var phoneData: List<PhoneData> = _phoneData
+
+    fun getPhoneData(idx: Int): PhoneData {
+        return phoneData[idx]
+    }
+
+    fun setPhoneData(data: ArrayList<PhoneData>?) {
+        if (data== null) return
+        _phoneData.clear()
+        for(i in data) {
+            _phoneData.add(PhoneData(i.name, i.checked))
+        }
+    }
+
+    fun setPhoneDataChecked(i: Int, state: Boolean){
+        _phoneData[i] = _phoneData[i].copy(checked = state)
+    }
+
     fun goNext(yy: Int, mm: Int, dd: Int, navToMain: () -> Unit) {
         val id = getidUsecase()
 
