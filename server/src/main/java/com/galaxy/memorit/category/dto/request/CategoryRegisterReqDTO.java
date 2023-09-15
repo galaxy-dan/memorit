e2bf2b0d816ed3b747c4351f9f467fb8e5b944a3
jhp336 <jhp336@naver.com>
2023-09-15 17:19:28 +0900

    feat: 응답을 위한 DTO 추가

---
+++ server/src/main/java/com/galaxy/memorit/category/dto/request/CategoryRegisterReqDTO.java
@@ -0,0 +1,12 @@
+package com.galaxy.memorit.category.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryRegisterReqDTO {
+	private String categoryName;
+}
