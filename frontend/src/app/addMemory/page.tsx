9c3582e5782e6c3a3524a7c52194e2549d47c2b4
ssh102525 <ssh102525@naver.com>
2023-09-15 14:14:29 +0900

    [S09P22A207-87] feat: 기억 추가 페이지 추가

--- frontend/src/app/addMemory/page.tsx
+++ frontend/src/app/addMemory/page.tsx
@@ -1,45 +1,100 @@
 'use client';
 
+import { useState } from 'react';
+
+import PictureInput from '@/components/pictureInput';
+import SearchInput from '@/components/searchInput';
+import TextInput from '@/components/textInput';
 import SelectButton from '@/components/selectButton';
 import SelectButtonGroup from '@/components/selectButtonGroup';
-import { useState } from 'react';
-import { IoMdClose } from 'react-icons/io';
+import TextareaInput from '@/components/textareaInput';
 
+import { IoMdClose } from 'react-icons/io';
+import { BsPeople, BsPerson } from 'react-icons/bs';
+import { BiCategory } from 'react-icons/bi';
+import { MdOutlineAttachMoney } from 'react-icons/md';
+import { AiOutlineGift } from 'react-icons/ai';
+import { CgNotes } from 'react-icons/cg';
+
+import { motion } from 'framer-motion';
 export default function AddMemoryPage() {
-  const [send, setSend] = useState<boolean>(true);
+  const [isSend, setIsSend] = useState<boolean>(true);
 
   function setSendTrue() {
-    setSend(true);
+    setIsSend(true);
   }
 
   function setSendFalse() {
-    setSend(false);
+    setIsSend(false);
   }
 
   return (
-    <>
+    <div className="bg-neutral-200 w-full min-h-screen">
       {/* 상단 탭 부분 */}
-      <div className="flex justify-between items-center w-full py-5 px-3">
-        <button className="p-2"><IoMdClose className="text-2xl" /></button>
+      <div className="flex justify-between items-center w-full py-5 px-5">
+        <motion.button 
+          className="rounded-full p-1"
+          initial = {false}
+          whileTap={{
+            backgroundColor: "#D0D0D0",
+          }}
+          transition={{
+            duration: 0.7,
+            scale: {
+              type: 'spring',
+              damping: 5,
+              stiffness: 100,
+              restDelta: 0.001,
+            },
+          }}
+          >
+          <IoMdClose className="text-2xl" />
+        </motion.button>
         <p className="text-2xl">기억 더하기</p>
-        <button className="text-red-400 p-2">저장</button>
+        <motion.button className="text-red-400 p-1"
+        initial = {false}
+        whileTap={{
+          backgroundColor: "#D0D0D0",
+        }}
+        transition={{
+          duration: 0.7,
+          scale: {
+            type: 'spring',
+            damping: 5,
+            stiffness: 100,
+            restDelta: 0.001,
+          },
+        }}
+        >저장</motion.button>
       </div>
 
       {/* 송수신 선택 부분 */}
+
       <SelectButtonGroup>
         <SelectButton
-          isSelected={send}
+          isSelected={isSend}
           position={'l'}
           text={'보낸 기억'}
           onClickFunction={setSendTrue}
         />
         <SelectButton
-          isSelected={!send}
+          isSelected={!isSend}
           position={'r'}
           text={'받은 기억'}
           onClickFunction={setSendFalse}
         />
       </SelectButtonGroup>
-    </>
+
+      <SearchInput type={'category'} icon={<BiCategory />} placeholder="카테고리" />
+      <hr className="border border-neutral-300 my-2"/>
+      <TextInput icon={<MdOutlineAttachMoney />} placeholder="금액" />
+      <TextInput icon={<AiOutlineGift />} placeholder="선물" />
+      <hr className="border border-neutral-300  my-2"/>
+      <SearchInput type={'name'} icon={<BsPerson />} placeholder="이름" />
+      <TextInput icon={<BsPeople />} placeholder="관계" />
+      <hr className="border border-neutral-300  my-2"/>
+      <TextareaInput icon={<CgNotes/>} placeholder="메모" />
+      <PictureInput />
+    </div>
   );
 }
