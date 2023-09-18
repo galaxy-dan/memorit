6945a90eabeca829cb6ef7833775491e32935608
jhp336 <jhp336@naver.com>
2023-09-18 12:27:04 +0900

    [S09P22A207-128] feat: 카테고리 조회 API 추가

--- server/src/main/java/com/galaxy/memorit/category/application/service/implement/CategoryServiceImpl.java
+++ server/src/main/java/com/galaxy/memorit/category/application/service/implement/CategoryServiceImpl.java
@@ -1,12 +1,19 @@
 package com.galaxy.memorit.category.application.service.implement;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 import com.galaxy.memorit.category.application.service.CategoryService;
 import com.galaxy.memorit.category.domain.entity.UserCategory;
 import com.galaxy.memorit.category.dto.request.CategoryRegisterReqDTO;
+import com.galaxy.memorit.category.dto.response.CategoryResDTO;
+import com.galaxy.memorit.category.infrastructure.persistence.entity.BaseCategoryEntity;
 import com.galaxy.memorit.category.infrastructure.persistence.entity.UserCategoryEntity;
 import com.galaxy.memorit.category.infrastructure.persistence.mapper.CategoryMapper;
+import com.galaxy.memorit.category.infrastructure.persistence.repository.BaseCategoryRepository;
 import com.galaxy.memorit.category.infrastructure.persistence.repository.UserCategoryRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -15,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
 	private final UserCategoryRepository userCategoryRepository;
+	private final BaseCategoryRepository baseCategoryRepository;
 	private final CategoryMapper categoryMapper;
 	@Override
 	public void registerCategory(String userId, CategoryRegisterReqDTO dto) {
@@ -22,8 +30,20 @@ public class CategoryServiceImpl implements CategoryService {
 			.userId(userId)
 			.categoryName(dto.getCategoryName())
 			.build();
-		System.out.println(userCategory.getUserId());
-		System.out.println(userCategory.getCategoryName());
 		userCategoryRepository.save(categoryMapper.toUserCategoryEntity(userCategory));
+	}
+
+	@Override
+	public CategoryResDTO getCategory(String userId) {
+		//base와 user 커스텀 카테고리 조회
+		List<String> categoryList = baseCategoryRepository.findAll()
+			.stream()
+			.map(BaseCategoryEntity::getCategoryName)
+			.collect(Collectors.toList());
+		categoryList.addAll(userCategoryRepository.findAllByUserId(categoryMapper.stringToUUID(userId))
+			.stream()
+			.map(UserCategoryEntity::getCategoryName)
+			.collect(Collectors.toList()));
+		return new CategoryResDTO(categoryList);
 	}
 }
--- server/src/main/java/com/galaxy/memorit/category/infrastructure/persistence/repository/UserCategoryRepository.java
+++ server/src/main/java/com/galaxy/memorit/category/infrastructure/persistence/repository/UserCategoryRepository.java
@@ -1,8 +1,12 @@
 package com.galaxy.memorit.category.infrastructure.persistence.repository;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.galaxy.memorit.category.infrastructure.persistence.entity.UserCategoryEntity;
 
 public interface UserCategoryRepository extends JpaRepository<UserCategoryEntity,Integer> {
+	List<UserCategoryEntity> findAllByUserId(UUID userId);
 }
