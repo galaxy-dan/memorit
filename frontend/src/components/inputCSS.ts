f379c80ab3be95e14cdc9a108e1c34a63bc9186a
ssh102525 <ssh102525@naver.com>
2023-09-18 12:16:11 +0900

    [S09P22A207-87] rename: inputCSS.ts 파일 위치 변경

--- frontend/src/components/inputCSS.ts
+++ frontend/src/components/inputCSS.ts
@@ -2,17 +2,18 @@ export function transitionCss(): string {
   return 'transition ease-in-out duration-[150ms] ';
 }
 
+export const transitionCss2 = 'transition ease-in-out duration-[150ms] ';
+
 export function transitionCssSlower(): string {
   return 'transition ease-in-out duration-[1000ms] ';
 }
 
 export function iconCss(cond1: boolean, cond2: boolean): string {
-  return (
-    `text-3xl ${(cond1 || cond2) && 'text-red-200 '}` +
-    transitionCss() +
-    ' mr-2'
-  );
+  return ` mr-2 text-3xl ${
+    (cond1 || cond2) && 'text-red-200 '
+  } ${transitionCss()} `;
 }
+
 export function containerCss(): string {
   return 'mx-2 px-3 py-2 border rounded-xl bg-white my-[0.65rem]';
 }
