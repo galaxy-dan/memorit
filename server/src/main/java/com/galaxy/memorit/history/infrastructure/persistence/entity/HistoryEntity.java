705ba5c3a240c0ba1e9fed990dc51fce30164893
jhp336 <jhp336@naver.com>
2023-09-20 11:04:25 +0900

    feat: 기록 도메인 및 엔티티 생성

--- server/src/main/java/com/galaxy/memorit/history/infrastructure/persistence/entity/HistoryEntity.java
+++ server/src/main/java/com/galaxy/memorit/history/infrastructure/persistence/entity/HistoryEntity.java
@@ -1,4 +1,48 @@
 package com.galaxy.memorit.history.infrastructure.persistence.entity;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "friend")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class HistoryEntity {
+	@Id
+	@Column(name = "article_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "user_id", columnDefinition = "BINARY(16)", nullable = false)
+	private UUID userId;
+	@Column(name = "friend_id", columnDefinition = "BINARY(16)", nullable = false)
+	private UUID friendId;
+	@Column(name = "date", nullable = false)
+	private LocalDate date;
+	@Column(name = "type", nullable = false)
+	private String type;
+	@Column(name = "amount")
+	private Integer amount;
+	@Column(name = "item")
+	private String item;
+	@Column(name = "detail")
+	private String detail;
+	@Column(name = "image")
+	private String image;
+	@Column(name = "is_given", nullable = false)
+	private Boolean is_given;
 }
