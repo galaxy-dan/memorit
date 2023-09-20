d17da48fb85303f206ad206f7acc2ab96ab9ef9d
jhp336 <jhp336@naver.com>
2023-09-20 14:09:52 +0900

    [S09P22A207-61] feat: 기록 작성 API 추가

--- server/src/main/java/com/galaxy/memorit/history/application/service/HistoryService.java
+++ server/src/main/java/com/galaxy/memorit/history/application/service/HistoryService.java
@@ -1,4 +1,7 @@
 package com.galaxy.memorit.history.application.service;
 
+import com.galaxy.memorit.history.dto.request.HistoryCreateReqDTO;
+
 public interface HistoryService {
+	void createHistory(String userId, HistoryCreateReqDTO dto);
 }
--- server/src/main/java/com/galaxy/memorit/history/presentation/controller/HistoryController.java
+++ server/src/main/java/com/galaxy/memorit/history/presentation/controller/HistoryController.java
@@ -1,4 +1,33 @@
 package com.galaxy.memorit.history.presentation.controller;
 
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.galaxy.memorit.history.application.service.HistoryService;
+import com.galaxy.memorit.history.dto.request.HistoryCreateReqDTO;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/history")
+@RequiredArgsConstructor
 public class HistoryController {
+	private final HistoryService historyService;
+
+	@PostMapping
+	public ResponseEntity<Void> createHistory(@RequestBody HistoryCreateReqDTO dto){
+		//이게 찐
+		//historyService.createHistory(authentication.getName(), dto);
+
+		//테스트용
+		String uuid = "99d7f4dd55244c54a523032169193f40";
+		historyService.createHistory(uuid, dto);
+
+		return ResponseEntity.created(URI.create("")).build();
+	}
 }
