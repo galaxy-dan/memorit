b3e3b11b7916ac02fdf11091e79767bc5b53c524
JeonJungyu <wjswnsrb93@naver.com>
2023-09-22 16:48:22 +0900

    style: 친구 목록 페이지 css 수정

--- frontend/src/app/friend/page.tsx
+++ frontend/src/app/friend/page.tsx
@@ -2,11 +2,26 @@
 import { get } from '@/service/api/http';
 import { useQuery } from '@tanstack/react-query';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import RedHeart from 'public/icons/red-heart.svg';
+import { MdFilterList } from 'react-icons/md';
 
-const categoryList = ['전체', '직장', '고등학교 친구', '친구'];
+const categoryList = [
+  '전체',
+  '직장',
+  '고등학교 친구',
+  '친구',
+  '가족',
+  '동료',
+  '직장',
+  '고등학교 친구',
+  '친구',
+  '가족',
+  '동료',
+];
 
 export default function FriendPage() {
+  const router = useRouter();
   const { data } = useQuery({
     queryKey: ['category'],
     queryFn: () => get('/category'),
@@ -14,25 +29,38 @@ export default function FriendPage() {
   });
 
   return (
-    <div>
-      <p className='text-3xl font-bold text-center my-16'>친구 목록</p>
-      <div className="">
+    <div className="flex flex-col  px-4">
+      <p className="text-3xl font-bold text-center my-16">친구 목록</p>
+      <div className="w-full overflow-auto scrollbar-hide">
         <div className="flex gap-3">
           {categoryList.map((el, index) => (
-            <div key={el} className="bg-red-50 px-6 py-2 rounded-xl shadow-sm">
+            <div
+              key={el}
+              className="bg-red-50 px-6 py-2 rounded-xl shadow-sm whitespace-nowrap"
+            >
               {el}
             </div>
           ))}
         </div>
       </div>
 
-      <div className='flex justify-end'>
-        <p>이름 순</p>
+      <div className="flex justify-end py-5">
+        <div className="flex items-center gap-2">
+          <MdFilterList size="20" />
+          <p className="font-medium text-sm">이름 순</p>
+        </div>
       </div>
 
       <div className="grid grid-cols-3">
-        {[1, 2, 3, 4, 5, 6].map((el) => (
-          <div key={el} className="border-2 border-green-500 rounded-lg px-2 m-2 shadow-md">
+        {[
+          1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5,
+          6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6,
+        ].map((el, index) => (
+          <div
+            key={index}
+            className="border-2 border-green-500 rounded-lg px-2 m-2 shadow-md cursor-pointer"
+            onClick={() => router.push(`/friend/${el}`)}
+          >
             <div className="flex justify-end items-center gap-1">
               <Image src={RedHeart} alt={'red'} width={'15'} />
               <p className="font-bold">10</p>
