0ea0527f31ac131adbaf0b50f5a3526ab33a0451
JeonJungyu <wjswnsrb93@naver.com>
2023-09-22 16:47:56 +0900

    style: 친구 상세 페이지 css 수정

--- frontend/src/app/friend/[slug]/page.tsx
+++ frontend/src/app/friend/[slug]/page.tsx
@@ -20,52 +20,48 @@ export default function FriendDetailPage() {
   });
 
   return (
-    <>
-      <div className="flex flex-col min-h-screen">
-        <div className="sticky top-0 flex items-center justify-between pt-6 px-6 bg-white z-10">
-          <div className="flex items-center">
-            <MdOutlineChevronLeft size="50" />
-            <p className="text-3xl font-bold">김하연</p>
-          </div>
-          <div className="flex items-center gap-2">
-            <Image src={WriteIcon} alt={'write'} width={'18'} />
-            <Image src={DeleteIcon} alt={'delete'} width={'24'} />
-          </div>
+    <div className="flex flex-col ">
+      <div className="sticky top-0 flex items-center justify-between pt-6 px-6 bg-white z-10">
+        <div className="flex items-center">
+          <MdOutlineChevronLeft size="50" />
+          <p className="text-3xl font-bold">김하연</p>
         </div>
-        <div className="flex justify-evenly pt-9">
-          <Image src={RedHeart} alt={'red'} width={'73'} />
-          <div className="flex flex-col justify-center items-center">
-            <p className="text-lg font-bold">직장</p>
-            <p className="text-sm">관계</p>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <p className="text-lg font-bold">57K</p>
-            <p className="text-sm">나눈 기억</p>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <p className="text-lg font-bold">100만</p>
-            <p className="text-sm">기억의 크기</p>
-          </div>
+        <div className="flex items-center gap-2">
+          <Image src={WriteIcon} alt={'write'} width={'18'} />
+          <Image src={DeleteIcon} alt={'delete'} width={'24'} />
         </div>
-        <div className="my-5">
-          <div className="flex flex-col border-2 border-gray-300 mx-5 px-6 py-5 rounded-xl gap-2">
-            <p className="text-3xl font-bold">Red Heart</p>
-            <p className="text-sm font-bold">
-              앞으로 어떤 관계가 될지 기대돼요!
-            </p>
-            <meter
-              className="w-full"
-              min="0"
-              max="100"
-              low={20}
-              high={65}
-              optimum={15}
-              value={'60'}
-            />
-          </div>
-        </div>
-        <History />
       </div>
-    </>
+      <div className="flex justify-evenly pt-9">
+        <Image src={RedHeart} alt={'red'} width={'73'} />
+        <div className="flex flex-col justify-center items-center">
+          <p className="text-lg font-bold">직장</p>
+          <p className="text-sm">관계</p>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <p className="text-lg font-bold">57K</p>
+          <p className="text-sm">나눈 기억</p>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <p className="text-lg font-bold">100만</p>
+          <p className="text-sm">기억의 크기</p>
+        </div>
+      </div>
+      <div className="my-5">
+        <div className="flex flex-col border-2 border-gray-300 mx-5 px-6 py-5 rounded-xl gap-2">
+          <p className="text-3xl font-bold">Red Heart</p>
+          <p className="text-sm font-bold">앞으로 어떤 관계가 될지 기대돼요!</p>
+          <meter
+            className="w-full"
+            min="0"
+            max="100"
+            low={20}
+            high={65}
+            optimum={15}
+            value={'60'}
+          />
+        </div>
+      </div>
+      <History />
+    </div>
   );
 }
