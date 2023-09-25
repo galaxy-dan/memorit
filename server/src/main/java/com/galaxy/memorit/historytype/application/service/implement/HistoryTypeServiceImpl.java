301170f5ccbb988ba8f6fa62b1f4d606dab5e9f0
jhp336 <jhp336@naver.com>
2023-09-25 10:48:27 +0900

    [S09P22A207-206] feat: 경조사 타입 검색 API 추가

--- server/src/main/java/com/galaxy/memorit/historytype/application/service/implement/HistoryTypeServiceImpl.java
+++ server/src/main/java/com/galaxy/memorit/historytype/application/service/implement/HistoryTypeServiceImpl.java
@@ -2,21 +2,18 @@ package com.galaxy.memorit.historytype.application.service.implement;
 
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.galaxy.memorit.common.exception.NoSuchUserException;
 import com.galaxy.memorit.historytype.application.service.HistoryTypeService;
-import com.galaxy.memorit.historytype.domain.entity.UserHistoryType;
 import com.galaxy.memorit.historytype.dto.request.HistoryTypeRegisterReqDTO;
+import com.galaxy.memorit.historytype.dto.request.HistoryTypeSearchReqDTO;
 import com.galaxy.memorit.historytype.dto.response.HistoryTypeResDTO;
-import com.galaxy.memorit.historytype.infrastructure.persistence.entity.BaseHistoryTypeEntity;
-import com.galaxy.memorit.historytype.infrastructure.persistence.entity.UserHistoryTypeEntity;
+import com.galaxy.memorit.historytype.infrastructure.persistence.entity.HistoryTypeEntity;
 import com.galaxy.memorit.historytype.infrastructure.persistence.mapper.HistoryTypeMapper;
-import com.galaxy.memorit.historytype.infrastructure.persistence.repository.BaseHistoryTypeRepository;
-import com.galaxy.memorit.historytype.infrastructure.persistence.repository.UserHistoryTypeRepository;
+import com.galaxy.memorit.historytype.infrastructure.persistence.repository.HistoryTypeRepository;
 import com.galaxy.memorit.user.domain.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -24,8 +21,7 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class HistoryTypeServiceImpl implements HistoryTypeService {
-	private final UserHistoryTypeRepository userHistoryTypeRepository;
-	private final BaseHistoryTypeRepository baseHistoryTypeRepository;
+	private final HistoryTypeRepository historyTypeRepository;
 	private final HistoryTypeMapper historyTypeMapper;
 	private final UserRepository userRepository;
 	@Transactional
@@ -34,27 +30,20 @@ public class HistoryTypeServiceImpl implements HistoryTypeService {
 		UUID userUUID = historyTypeMapper.stringToUUID(userId);
 		userRepository.findById(userUUID).orElseThrow(NoSuchUserException::new);
 
-		UserHistoryTypeEntity userHistoryTypeEntity = UserHistoryTypeEntity.builder()
+		HistoryTypeEntity historyTypeEntity = HistoryTypeEntity.builder()
 			.userId(userUUID)
 			.typeName(dto.getTypeName())
 			.build();
-		userHistoryTypeRepository.save(userHistoryTypeEntity);
+		historyTypeRepository.save(historyTypeEntity);
 	}
 
 	@Transactional(readOnly = true)
 	@Override
-	public HistoryTypeResDTO getHistoryType(String userId) {
+	public HistoryTypeResDTO searchHistoryType(String userId, HistoryTypeSearchReqDTO dto) {
 		UUID userUUID = historyTypeMapper.stringToUUID(userId);
-		userRepository.findById(userUUID).orElseThrow(NoSuchUserException::new);
-
-		List<String> typeList = baseHistoryTypeRepository.findAll()
-			.stream()
-			.map(BaseHistoryTypeEntity::getTypeName)
-			.collect(Collectors.toList());
-		typeList.addAll(userHistoryTypeRepository.findAllByUserId(userUUID)
-			.stream()
-			.map(UserHistoryTypeEntity::getTypeName)
-			.collect(Collectors.toList()));
+		//userRepository.findById(userUUID).orElseThrow(NoSuchUserException::new);
+		System.out.println(dto.getKeyword());
+		List<String> typeList = historyTypeRepository.searchHistoryTypes(userUUID, dto);
 		return new HistoryTypeResDTO(typeList);
 	}
 }
