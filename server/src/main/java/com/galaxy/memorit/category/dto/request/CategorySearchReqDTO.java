c4856c1ce03cf019439be392089c853f12a53c32
jhp336 <jhp336@naver.com>
2023-09-26 11:26:18 +0900

    [S09P22A207-213] feat: 친구 카테고리 조회 API를 위한 DTO 추가

---
+++ server/src/main/java/com/galaxy/memorit/category/dto/request/CategorySearchReqDTO.java
@@ -0,0 +1,14 @@
+package com.galaxy.memorit.category.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategorySearchReqDTO {
+	private String keyword;
+}
--- server/src/main/java/com/galaxy/memorit/category/infrastructure/persistence/repository/BaseCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.galaxy.memorit.category.infrastructure.persistence.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.galaxy.memorit.category.infrastructure.persistence.entity.BaseCategoryEntity;
-@Repository
-public interface BaseCategoryRepository extends JpaRepository<BaseCategoryEntity, Long> {
-}
