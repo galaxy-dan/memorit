bd21be3a53b494660ef432e85d7b1b52ac2c455d
jhp336 <jhp336@naver.com>
2023-09-26 14:08:38 +0900

    feat: csrf 설정 제거

--- server/src/main/java/com/galaxy/memorit/common/config/security/SecurityConfig.java
+++ server/src/main/java/com/galaxy/memorit/common/config/security/SecurityConfig.java
@@ -6,10 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
@@ -20,6 +21,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
         http
             .authorizeHttpRequests((authz) -> authz
                 //.anyRequest().authenticated()
