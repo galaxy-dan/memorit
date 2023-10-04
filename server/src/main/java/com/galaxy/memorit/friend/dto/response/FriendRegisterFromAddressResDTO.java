417b439beebd296e1d7f53d491b9db80e8718916
jhp336 <jhp336@naver.com>
2023-10-04 09:03:21 +0900

    refactor: 클래스명 변경

--- server/src/main/java/com/galaxy/memorit/friend/dto/response/FriendRegisterFromAddressResDTO.java
+++ server/src/main/java/com/galaxy/memorit/friend/dto/response/FriendRegisterFromAddressResDTO.java
@@ -7,6 +7,6 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class FriendRegisterResDTO {
+public class FriendRegisterFromAddressResDTO {
 	private int code;
 }
