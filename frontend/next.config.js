cd0fbfe38565ef6020283f5802720de0d3cfed27
JeonJungyu-1 <wjswnsrb93@naver.com>
2024-12-31 00:15:27 +0900

    build: react compiler 설정 추가

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
   images: {
     domains: ['mermorit.s3.ap-northeast-2.amazonaws.com'],
   },
+  experimental: {
+    reactCompiler: true,
+  },
 };
 
 module.exports = nextConfig;
